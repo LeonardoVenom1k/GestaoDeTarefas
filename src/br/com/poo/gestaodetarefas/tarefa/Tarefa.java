95f1fd34def835e67b115ddb1f92118d47dbbf49
LeonardoVenom1k <leonardovenom9@gmail.com>
2025-05-28 22:46:22 -0300

    Inicialização do projeto

---
+++ src/br/com/poo/gestaodetarefas/tarefa/Tarefa.java
@@ -0,0 +1,5 @@
+package br.com.poo.gestaodetarefas.tarefa;
+
+public class Tarefa {
+
+}
---
+++ src/br/com/poo/gestaodetarefas/teste/TesteTarefa.java
@@ -0,0 +1,7 @@
+package br.com.poo.gestaodetarefas.teste;
+
+public class TesteTarefa {
+    public static void main(String[] args) {
+
+    }
+}
