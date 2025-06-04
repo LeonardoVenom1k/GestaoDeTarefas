4801d4c6e7ee5106a1cac76f55666c955148d91e
LeonardoVenom1k <leonardovenom9@gmail.com>
2025-06-03 22:46:22 -0300

    Implementado exibição de detalhes da tarefa

--- src/br/com/poo/gestaodetarefas/teste/TesteTarefa.java
+++ src/br/com/poo/gestaodetarefas/teste/TesteTarefa.java
@@ -1,7 +1,13 @@
 package br.com.poo.gestaodetarefas.teste;
 
+import br.com.poo.gestaodetarefas.tarefa.Tarefa;
+
 public class TesteTarefa {
     public static void main(String[] args) {
+
+        Tarefa tarefa = new Tarefa(1, "Entrega 2 do PID", false);
+
+        System.out.println(tarefa.obterDados());
 
     }
 }
